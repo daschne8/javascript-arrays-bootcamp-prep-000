3fef9edc46a8b71e22c4fbf4764955db88f8d235
Daniel Schneider <daschne8@gmail.com>
2019-01-09 22:09:50 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,9 @@
 var chocolateBars = ['snickers','hundred grand','kitkat','skittles']
+function addElementToBeginningOfArray(array,element){
+  newArray = [element, ...array]
+  return array.unshift(element)
+}
+
+function destructivelyAddElementToEndOfArray(array,element){
+  return array.unshift(element)
+}
